acfbe4896ff07fcfdcf087bef7fdac32d2c5d8b5
Charlie <hammersleycharlie@gmail.com>
2026-05-03 16:14:23 -0400

    Update Form Options
    Added bot info and comments to the form for quicker scouting information before pit scouting was concluded.

--- src/components/matchScoutingForm.tsx
+++ src/components/matchScoutingForm.tsx
@@ -55,7 +55,21 @@ export default function matchScoutingForm() {
     const [climbTimeTeleop, setClimbTimeTeleop] = useState(0);
     const [climbLevelTeleop, setClimbLevelTeleop] = useState(0);
 
-    // ADD ROBOT INFORMATION TO FORM
+    // Robot Info
+    const [robotType, setRobotType] = useState('');
+    const [driveTrain, setDriveTrain] = useState('');
+    const [overBump, setOverBump] = useState(Boolean);
+    const [underTrench, setUnderTrench] = useState(Boolean);
+
+    // Comments
+    const [driverSkill, setDriverSkill] = useState(0);
+    const [defenseSkill, setDefenseSkill] = useState(0);
+    const [robotSpeed, setRobotSpeed] = useState(0);
+    const [stability, setStability] = useState(0);
+    const [intakeConsistency, setIntakeConsistency] = useState(0);
+    const [scoringConsistency, setScoringConsistency] = useState(0);
+    const [otherComments, setOtherComments] = useState('');
+
 
     if(step == 'opener') {
         return (
@@ -82,7 +96,16 @@ export default function matchScoutingForm() {
                     
                     <h2>Match Information</h2>
                         <p>Match Number</p> <input type="number" value={matchNumber} onChange={(e) => setMatch(e.target.valueAsNumber)} required />
-                        <p>Team Position</p> <input value={teamPosition} onChange={(e) => setPosition(e.target.value)} />
+                        <p>Team Position</p> <select value={teamPosition} onChange={(e) => setPosition(e.target.value)} required>
+                            <option value="">Select Team Position</option>
+                            <option value={"Red 1"}>Red 1</option>
+                            <option value={"Red 2"}>Red 2</option>
+                            <option value={"Red 3"}>Red 3</option>
+                            <option value={"Blue 1"}>Blue 1</option>
+                            <option value={"Blue 2"}>Blue 2</option>
+                            <option value={"Blue 3"}>Blue 3</option>
+                        </select>
+
                         <p>Team Number</p> <input type="number" value={team} onChange={(e) => setTeam(e.target.value)} required />
                     <br />
 
@@ -106,6 +129,35 @@ export default function matchScoutingForm() {
                     <h2>Endgame</h2>
                         <p>Climb Level</p> <input type="number" value={climbLevelTeleop} onChange={(e) => setClimbLevelTeleop(e.target.valueAsNumber)} required />
                         <p>Climb Time</p> <input type="number" value={climbTimeTeleop} onChange={(e) => setClimbTimeTeleop(e.target.valueAsNumber)} required />
+                    <br />
+
+                    <h2>Robot Information</h2>
+                        <p>Robot Type</p> <select value={robotType} onChange={(e) => setRobotType(e.target.value)} required>
+                            <option value="">Select Robot Type</option>
+                            <option value={"Dumper"}>Dumper</option>
+                            <option value={"Turret"}>Turret</option>
+                            <option value={"Two-Turret"}>Two-Turret</option>
+                            <option value={"Single Lane"}>Single Lane</option>
+                            <option value={"Multi-Lane"}>Multi-Lane</option>
+                        </select>
+                        <p>Drive Train</p> <select value={driveTrain} onChange={(e) => setDriveTrain(e.target.value)} required>
+                            <option value="">Select Drivetrain</option>
+                            <option value={"Swerve"}>Swerve</option>
+                            <option value={"Tank"}>Tank</option>
+                            <option value={"Other"}>Other</option>
+                        </select>
+                        <p>Over Bump</p> <input type="checkbox" checked={overBump} onChange={(e) => setOverBump(e.target.checked)} />
+                        <p>Under Trench</p> <input type="checkbox" checked={underTrench} onChange={(e) => setUnderTrench(e.target.checked)} />
+                    <br />
+
+                    <h2>Comments</h2>
+                        <p>Driver Skill</p> <input type="number" min="0" max="10" value={driverSkill} onChange={(e) => setDriverSkill(e.target.valueAsNumber)} required />
+                        <p>Defense Skill</p> <input type="number" min="0" max="10" value={defenseSkill} onChange={(e) => setDefenseSkill(e.target.valueAsNumber)} required />
+                        <p>Robot Speed</p> <input type="number" min="0" max="10" value={robotSpeed} onChange={(e) => setRobotSpeed(e.target.valueAsNumber)} required />
+                        <p>Robot Stability</p> <input type="number" min="0" max="10" value={stability} onChange={(e) => setStability(e.target.valueAsNumber)} required />
+                        <p>Intake Consistency</p> <input type="number" min="0" max="10" value={intakeConsistency} onChange={(e) => setIntakeConsistency(e.target.valueAsNumber)} required />
+                        <p>Scoring Consistency</p> <input type="number" min="0" max="10" value={scoringConsistency} onChange={(e) => setScoringConsistency(e.target.valueAsNumber)} required />
+                        <p>Additional Comments</p> <input type="string" value={otherComments} onChange={(e) => setOtherComments(e.target.value)} required />
                     <br />
 
                     <button onClick={saveMatch}>Save Match</button>
